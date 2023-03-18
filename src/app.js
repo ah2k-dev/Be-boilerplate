9831fa8265c08619dc6a476a7ab2bffe42bd3c97
Ahmed Hassan <ah2k.dev@gmail.com>
2023-03-18 17:15:37 +0500

    Integrating swagger api documentation
    Started

--- src/app.js
+++ src/app.js
@@ -5,13 +5,34 @@ const ApiError = require("./utils/ApiError");
 const app = express();
 const { auth } = require("./router");
 const loggerMiddleware = require("./middleware/loggerMiddleware");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+const apiDoc = require("./apiDoc");
 
+// Swagger configuration options
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Boilerplate API",
+      version: "1.1",
+    },
+  },
+  // Path to the API routes files
+  apis: ["./src/apiDoc.js"],
+};
+
+// Generate Swagger specification
+const swaggerSpec = swaggerJsDoc(swaggerOptions);
+
+// Middlewares
 app.use(express.json());
 app.use(cors());
 app.options("*", cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(loggerMiddleware);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 //routes
 app.use("/auth", auth);
